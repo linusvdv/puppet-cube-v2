c245a2decab2df29a2b8999aed430307f553a1fc
linusvdv <30822552+linusvdv@users.noreply.github.com>
2024-05-03 20:19:15 +0200

    Change rendering from SFML to GLFW and Glad rendering 2 triangles

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,185 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
+#include <iostream>
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+
+// vertex shader
+const char *vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+
+// fragment shader
+const char *fragmentShaderSource = 
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+
+static void error_callback(int error, const char* description) {
+    std::cerr << "ERROR: " << description << std::endl;
+}
+
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 
 int main (int argc, char *argv[]) {
-    sf::RenderWindow window = sf::RenderWindow{ { 1000, 1000 }, "Puppet Cube V2" };
-    window.setPosition({0, 0});
-    window.setFramerateLimit(60);
+    glfwSetErrorCallback(error_callback);
 
-    sf::CircleShape circle(100.f);
-    circle.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        for (sf::Event event = sf::Event{}; window.pollEvent(event);) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-            if (event.type == sf::Event::Resized) {
-                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
-                window.setView(sf::View(visibleArea));
-            }
-        }
-
-        window.clear();
-
-        window.draw(circle);
-
-        window.display();
+    // initialising GLFW
+    if (!glfwInit()) {
+        return -1;
     }
+
+    // create window with OpenGL 3.x or higher
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Puppet Cube V2", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glfwMakeContextCurrent(window);
+
+
+    // initialising GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Faild to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+
+    // vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int successVertexShader;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &successVertexShader);
+    if (!successVertexShader) {
+        char vertexShaderInfoLog[512];
+        glGetShaderInfoLog(vertexShader, 512, NULL, vertexShaderInfoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vertexShaderInfoLog << std::endl;
+    }
+
+
+    // fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    int successFragmentShader;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &successFragmentShader);
+    if (!successFragmentShader) {
+        char fragmentShaderInfoLog[512];
+        glGetShaderInfoLog(fragmentShader, 512, NULL, fragmentShaderInfoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fragmentShaderInfoLog << std::endl;
+    }
+
+
+    // link shader program
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int successShaderProgram;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &successShaderProgram);
+    if (!successShaderProgram) {
+        char shaderProgramInfoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, NULL, shaderProgramInfoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << shaderProgramInfoLog << std::endl;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    // drawing two triangle
+    float vertices[] = {
+         0.5f,  0.5f,  0.0f,
+         0.5f, -0.5f,  0.0f,
+        -0.0f, -0.5f,  0.0f,
+        -0.0f,  0.5f,  0.0f
+    };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+
+
+    // vertex buffer objects
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    // vertex array object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+
+    // element buffer objects
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+    // linking vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+    // Wireframe mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (!glfwWindowShouldClose(window)) {
+        // clear background
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
