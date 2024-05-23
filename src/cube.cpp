6e1e7c4f13cdaf62638e3e0ffa63aaa0f453fb3b
linusvdv <30822552+linusvdv@users.noreply.github.com>
2024-05-23 21:53:55 +0200

    finishing mesh of cube and enable complete rendering

--- src/cube.cpp
+++ src/cube.cpp
@@ -5,124 +5,11 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
 
 
 #include "cube.h"
 #include "shader.h"
-
-
-void Cube::MeshInitialisation() {
-    // corner two directions facing out
-    int current_triangle_size = meshes_[4].points.size() / 3;
-    const int num_edges_radius = 20; // multible of two
-    const float begin_angle = asin(1 / (sqrt(2) * 3));
-    const float end_angle = asin(1 / sqrt(2));
-    const float length = -0.2*sqrt(2)*3;
-
-    for (int i = 0; i < num_edges_radius/2; i++) {
-        float angle = begin_angle + (end_angle - begin_angle) / (num_edges_radius/2) * i;
-
-        // left bottom
-        meshes_[4].points.insert(meshes_[4].points.end(),
-                {float(length * cos(angle)), -0.2, float(length * sin(angle))});
-        // left top 
-        meshes_[4].points.insert(meshes_[4].points.end(),
-                {float(length * cos(angle)),  0.2, float(length * sin(angle))});
-        // right bottom
-        meshes_[4].points.insert(meshes_[4].points.end(),
-                {float(length * sin(angle)), -0.2, float(length * cos(angle))});
-        // right top
-        meshes_[4].points.insert(meshes_[4].points.end(),
-                {float(length * sin(angle)),  0.2, float(length * cos(angle))});
-
-        if (i == 0) {
-            continue;
-        }
-
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 + 2);
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 + 3);
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 - 1);
-
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 + 2);
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 - 2);
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 - 1);
-
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 + 3);
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 - 1);
-        meshes_[4].triangles[1].push_back(7);
-
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 + 2);
-        meshes_[4].triangles[1].push_back(current_triangle_size + i*4 - 2);
-        meshes_[4].triangles[1].push_back(5);
-
-
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 + 0);
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 + 1);
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 - 3);
-
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 + 0);
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 - 4);
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 - 3);
-
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 + 1);
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 - 3);
-        meshes_[4].triangles[2].push_back(7);
-
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 + 0);
-        meshes_[4].triangles[2].push_back(current_triangle_size + i*4 - 4);
-        meshes_[4].triangles[2].push_back(5);
-
-
-        meshes_[4].lines.push_back(current_triangle_size + i*4 - 4);
-        meshes_[4].lines.push_back(current_triangle_size + i*4 + 0);
-        meshes_[4].lines.push_back(current_triangle_size + i*4 - 2);
-        meshes_[4].lines.push_back(current_triangle_size + i*4 + 2);
-    }
-
-    // middle
-    meshes_[4].points.insert(meshes_[4].points.end(),
-            {float(length * cos(end_angle)), -0.2, float(length * sin(end_angle))});
-    meshes_[4].points.insert(meshes_[4].points.end(),
-            {float(length * cos(end_angle)), 0.2, float(length * sin(end_angle))});
-
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 + 1);
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 - 1);
-
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 - 2);
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 - 1);
-
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 + 1);
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 - 1);
-    meshes_[4].triangles[1].push_back(7);
-
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-    meshes_[4].triangles[1].push_back(current_triangle_size + num_edges_radius/2*4 - 2);
-    meshes_[4].triangles[1].push_back(5);
-
-
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 + 1);
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 - 3);
-
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 - 4);
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 - 3);
-
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 + 1);
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 - 3);
-    meshes_[4].triangles[2].push_back(7);
-
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-    meshes_[4].triangles[2].push_back(current_triangle_size + num_edges_radius/2*4 - 4);
-    meshes_[4].triangles[2].push_back(5);
-
-    meshes_[4].lines.push_back(current_triangle_size + num_edges_radius/2*4 - 4);
-    meshes_[4].lines.push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-    meshes_[4].lines.push_back(current_triangle_size + num_edges_radius/2*4 - 2);
-    meshes_[4].lines.push_back(current_triangle_size + num_edges_radius/2*4 + 0);
-}
 
 
 Cube::Cube(Shader shader) {
@@ -158,9 +45,9 @@ Cube::Cube(Shader shader) {
     rotations_location_ = glGetUniformLocation(shader.ID, "rotations");
     glUniformMatrix4fv(rotations_location_, kNumPieces, GL_FALSE, glm::value_ptr(rotations[0]));
 
-    MeshInitialisation();
+    MeshInitialisationOneCorner();
+    MeshInitialisationTwoCorners();
 }
-
 
 
 Cube::~Cube() {
@@ -170,9 +57,9 @@ Cube::~Cube() {
 }
 
 
-
 void Cube::Draw(Shader shader) const {
     for (int i = 0; i < pieces_.size(); i++) {
+
         Mesh mesh = meshes_[pieces_[i].type];
 
         // send the vertex buffer objects
@@ -195,7 +82,7 @@ void Cube::Draw(Shader shader) const {
             glDrawElements(GL_TRIANGLES, mesh.triangles[color_index].size(), GL_UNSIGNED_INT, 0);
         }
 
-
+        // black outline
         unsigned int piece_data = ( i ) |                       // piece index
                 ( Colors::kBlack << 5 ) |                       // color index
                 ( (int)pieces_[i].current_rotation << 8 );      // effected by current rotation
@@ -206,4 +93,198 @@ void Cube::Draw(Shader shader) const {
 
         glDrawElements(GL_LINES, mesh.lines.size(), GL_UNSIGNED_INT, 0);
     }
+}
+
+
+void Cube::MeshInitialisationOneCorner() {
+    // corner one direction facing out
+    int current_triangle_size = meshes_[3].points.size() / 3;
+    const int num_edges_radius_lower = 8; // from area to 45 degree yellow
+    const int num_edges_radius_middle= 8; // from area to 45 degree green
+    const float begin_angle = atan(1 / 1);
+    const float middle_angle = acos((1+2*sqrt(2)) / (sqrt(2)*3)); // 45 degree from corner
+    const float end_angle = asin(1 / (sqrt(2)*3)); // edge
+    const float length = 0.2*sqrt(2)*3;
+
+    for (int i = 0; i < num_edges_radius_lower; i++) {
+        float angle = begin_angle + (middle_angle - begin_angle) / (num_edges_radius_lower) * (i+1);
+
+        // points
+        meshes_[3].points.insert(meshes_[3].points.end(),
+                {-float(length * cos(angle)), float(length * sin(angle)), -0.2, // outer yellow
+                 -float(length * cos(angle)), float(length * sin(angle)),  0.2, // inner
+                 -float(length * cos(angle)), -0.2, float(length * sin(angle)), // outer green
+                 -float(length * cos(angle)),  0.2, float(length * sin(angle))}); // inner
+
+        // triangles
+        int last_batch = current_triangle_size + i*4;
+        meshes_[3].triangles[0].insert(meshes_[3].triangles[0].end(), // yellow
+                {last_batch + 0, last_batch - 4, 3, // outer
+                 last_batch + 0, last_batch + 1, last_batch - 4, // smooth
+                 last_batch + 1, last_batch - 4, last_batch - 3, // smooth
+                 last_batch + 1, last_batch - 3, 8}); // inner
+                                                      //
+        meshes_[3].triangles[2].insert(meshes_[3].triangles[2].end(), // orange
+                {last_batch + 2, last_batch - 2, 6, // outer
+                 last_batch + 2, last_batch + 3, last_batch - 2, // smooth
+                 last_batch + 3, last_batch - 2, last_batch - 1, // smooth
+                 last_batch + 3, last_batch - 1, 8}); // inner
+
+        // lines
+        meshes_[3].lines.insert(meshes_[3].lines.end(),
+                {last_batch + 0, last_batch - 4,
+                 last_batch + 2, last_batch - 2});
+    }
+
+    current_triangle_size = meshes_[3].points.size() / 3;
+    for (int i = 0; i < num_edges_radius_middle; i++) {
+        float angle = middle_angle + (end_angle - middle_angle) / (num_edges_radius_middle) * (i+1);
+
+        // points
+        meshes_[3].points.insert(meshes_[3].points.end(),
+                {-float(length * cos(angle)), float(length * sin(angle)), -0.2, // outer yellow
+                 -float(length * cos(angle)), float(length * sin(angle)),  0.2, // inner
+                 -float(length * cos(angle)), -0.2, float(length * sin(angle)), // outer green
+                 -float(length * cos(angle)),  0.2, float(length * sin(angle))}); // inner
+
+        // triangles
+        int last_batch = current_triangle_size + i*4;
+        meshes_[3].triangles[1].insert(meshes_[3].triangles[1].end(), // yellow side
+                {last_batch + 0, last_batch - 4, 3, // outer
+                 last_batch + 0, last_batch + 1, last_batch - 4, // smooth
+                 last_batch + 1, last_batch - 4, last_batch - 3, // smooth
+                 last_batch + 1, last_batch - 3, 8, // inner
+                 // orange side
+                 last_batch + 2, last_batch - 2, 6, // outer
+                 last_batch + 2, last_batch + 3, last_batch - 2, // smooth
+                 last_batch + 3, last_batch - 2, last_batch - 1, // smooth
+                 last_batch + 3, last_batch - 1, 8}); // inner
+
+        // lines
+        meshes_[3].lines.insert(meshes_[3].lines.end(),
+                {last_batch + 0, last_batch - 4,
+                 last_batch + 2, last_batch - 2});
+    }
+    current_triangle_size = meshes_[3].points.size() / 3;
+    meshes_[3].triangles[1].insert(meshes_[3].triangles[1].end(),
+            {current_triangle_size - 4, 3, 7,
+             current_triangle_size - 2, 6, 7});
+
+    current_triangle_size = meshes_[3].points.size() / 3;
+    const int num_square = 8;
+    for (int i = 0; i <= num_square; i++) { // both sides inclusive
+        float angle_i = end_angle - (2 * end_angle) / num_square * i;
+        for (int j = 0; j <= num_square; j++) {
+            float angle_j = end_angle - (2 * end_angle) / num_square * j;
+            meshes_[3].points.insert(meshes_[3].points.end(),
+                    {-float(length * std::max(cos(angle_i), cos(angle_j))), length * sin(angle_j), length * sin(angle_i)});
+
+            // area
+            int last_batch = meshes_[3].points.size() / 3 - 1;
+            if (j != 0 && i != 0) {
+                meshes_[3].triangles[1].insert(meshes_[3].triangles[1].end(),
+                        {last_batch, last_batch - 1, last_batch - num_square-1,
+                         last_batch - 1, last_batch - num_square-1, last_batch - num_square-2});
+            }
+            
+            // outer
+            if (j == num_square && i != 0) {
+                meshes_[3].triangles[1].insert(meshes_[3].triangles[1].end(),
+                        {last_batch, last_batch - num_square-1, 7});
+                meshes_[3].lines.insert(meshes_[3].lines.end(),
+                        {last_batch, last_batch - num_square-1});
+            }
+            if (i == num_square && j != 0) { 
+                meshes_[3].triangles[1].insert(meshes_[3].triangles[1].end(),
+                        {last_batch, last_batch - 1, 7});
+                meshes_[3].lines.insert(meshes_[3].lines.end(),
+                        {last_batch, last_batch - 1});
+            }
+
+            // inner
+            if (j == 0 && i > 1) {
+                meshes_[3].triangles[1].insert(meshes_[3].triangles[1].end(),
+                        {last_batch, last_batch - num_square-1, current_triangle_size});
+            }
+            if (i == 0 && j > 1) { 
+                meshes_[3].triangles[1].insert(meshes_[3].triangles[1].end(),
+                        {last_batch, last_batch - 1, current_triangle_size});
+            }
+        }
+    }
+
+    int last_batch = meshes_[3].points.size() / 3 - 1;
+    meshes_[3].lines.insert(meshes_[3].lines.end(),
+            {last_batch, 7});
+}
+
+
+void Cube::MeshInitialisationTwoCorners() {
+    // corner two directions facing out
+    int current_triangle_size = meshes_[4].points.size() / 3;
+    const int num_edges_radius = 20; // multible of two
+    const float begin_angle = asin(1 / (sqrt(2) * 3));
+    const float end_angle = asin(1 / sqrt(2));
+    const float length = -0.2*sqrt(2)*3;
+
+    for (int i = 0; i < num_edges_radius/2; i++) {
+        float angle = begin_angle + (end_angle - begin_angle) / (num_edges_radius/2) * i;
+
+        // left bottom
+        meshes_[4].points.insert(meshes_[4].points.end(),
+                {float(length * cos(angle)), -0.2, float(length * sin(angle))});
+        // left top 
+        meshes_[4].points.insert(meshes_[4].points.end(),
+                {float(length * cos(angle)),  0.2, float(length * sin(angle))});
+        // right bottom
+        meshes_[4].points.insert(meshes_[4].points.end(),
+                {float(length * sin(angle)), -0.2, float(length * cos(angle))});
+        // right top
+        meshes_[4].points.insert(meshes_[4].points.end(),
+                {float(length * sin(angle)),  0.2, float(length * cos(angle))});
+
+        if (i == 0) {
+            continue;
+        }
+
+        int last_batch = current_triangle_size + i*4;
+        meshes_[4].triangles[1].insert(meshes_[4].triangles[1].end(),
+                {last_batch + 2, last_batch + 3, last_batch - 1,
+                 last_batch + 2, last_batch - 2, last_batch - 1,
+                 last_batch + 3, last_batch - 1, 7,
+                 last_batch + 2, last_batch - 2, 5});
+
+        meshes_[4].triangles[2].insert(meshes_[4].triangles[2].end(),
+                {last_batch + 0, last_batch + 1, last_batch - 3,
+                 last_batch + 0, last_batch - 4, last_batch - 3,
+                 last_batch + 1, last_batch - 3, 7,
+                 last_batch + 0, last_batch - 4, 5});
+
+        meshes_[4].lines.insert(meshes_[4].lines.end(),
+                {last_batch - 4, last_batch + 0, 
+                 last_batch - 2, last_batch + 2,});
+    }
+
+    // middle
+    meshes_[4].points.insert(meshes_[4].points.end(),
+            {float(length * cos(end_angle)), -0.2, float(length * sin(end_angle))});
+    meshes_[4].points.insert(meshes_[4].points.end(),
+            {float(length * cos(end_angle)), 0.2, float(length * sin(end_angle))});
+
+    int last_batch = current_triangle_size + num_edges_radius/2*4;
+    meshes_[4].triangles[1].insert(meshes_[4].triangles[1].end(),
+            {last_batch + 0, last_batch + 1, last_batch - 1,
+             last_batch + 0, last_batch - 2, last_batch - 1,
+             last_batch + 1, last_batch - 1, 7,
+             last_batch + 0, last_batch - 2, 5});
+
+    meshes_[4].triangles[2].insert(meshes_[4].triangles[2].end(),
+            {last_batch + 0, last_batch + 1, last_batch - 3,
+             last_batch + 0, last_batch - 4, last_batch - 3,
+             last_batch + 1, last_batch - 3, 7,
+             last_batch + 0, last_batch - 4, 5});
+
+    meshes_[4].lines.insert(meshes_[4].lines.end(),
+            {last_batch - 4, last_batch + 0, 
+             last_batch - 2, last_batch + 0,});
 }
