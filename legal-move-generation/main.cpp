73996dbd5d1d4af189c3613881d3462964e98ed9
linusvdv <30822552+linusvdv@users.noreply.github.com>
2024-07-16 12:46:43 +0200

    12 GB using array of all positions instead of map

--- legal-move-generation/main.cpp
+++ legal-move-generation/main.cpp
@@ -1,4 +1,6 @@
+#include <sys/types.h>
 #include <array>
+#include <cassert>
 #include <cstdint>
 #include <map>
 #include <iostream>
@@ -77,8 +79,9 @@ const int kNumPieces = 8;
 
 
 struct PositionValue {
-    int shortest_solution;
-    std::array<bool, kNumRotations> legal_move;
+    bool found = false;
+    int shortest_solution = 0;
+    std::array<bool, kNumRotations> legal_move = {};
 };
 
 
@@ -111,21 +114,55 @@ void DebugPiecesOut (std::array<Piece, kNumPieces> pieces) {
     std::cout << std::endl;
 }
 
-std::array<std::array<int, 4>, kNumPieces> GetPositionIndex (std::array<Piece, kNumPieces> position) {
-    std::array<std::array<int, 4>, kNumPieces> position_index;
-    for (int i = 0; i < kNumPieces; i++) {
+
+const unsigned int kEightFac = 40320; // 8!
+const unsigned int kNumOrientations = 88179840; // 8! * 3^7
+
+
+unsigned int GetPositionHash (std::array<Piece, kNumPieces>& position) {
+    unsigned int hash = 0;
+    const std::map<std::array<int, 3>, unsigned int> corner_index = {
+        {{ 1,  1,  1}, 0},
+        {{-1,  1,  1}, 1},
+        {{ 1, -1,  1}, 2},
+        {{ 1,  1, -1}, 3},
+        {{-1,  1, -1}, 4},
+        {{-1, -1,  1}, 5},
+        {{ 1, -1, -1}, 6},
+        {{-1, -1, -1}, 7}
+    };
+
+    std::array<bool, kNumPieces> used;
+    used.fill(false);
+
+    unsigned int orientation_hash = 0;
+    for (int i = 0; i < kNumPieces-1; i++) {
+        // index compressed to 8!
+        // 0 to 8!-1
+        unsigned int corner = corner_index.find(position[i].position)->second;
+        unsigned int small_corner_index = 0;
+        for (int j = 0; j < corner; j++) {
+            small_corner_index += u_int(!used[j]);
+        }
+        used[corner] = true;
+        hash *= kNumPieces-i;
+        hash += small_corner_index;
+
+        // piece orientation
         int orientation = 0;
         for (; orientation < 3; orientation++) {
             if (position[i].orientation[orientation] != 0) {
                 break;
             }
         }
-        position_index[i] = {position[i].position[0],
-            position[i].position[1],
-            position[i].position[2],
-            orientation};
+        orientation_hash *= 3;
+        orientation_hash += orientation;
     }
-    return position_index;
+
+    hash += orientation_hash * kEightFac;
+
+    assert(hash < kNumOrientations);
+    return hash;
 }
 
 
@@ -141,11 +178,14 @@ int main() {
         {{-1, -1, -1}, { 1,  1,  1}, { 1,  0,  0}}      // White,  Green,  Red   
     }};
 
-    std::map<std::array<std::array<int, 4>, kNumPieces>, PositionValue> positions;
+    std::array<PositionValue, kNumOrientations> positions = {};
+
     std::queue<std::pair<int, std::array<Piece, kNumPieces>>> next_positions;
     next_positions.push({0, pieces});
-    std::array<std::array<int, 4>, kNumPieces> position_index = GetPositionIndex(pieces);
-    positions[position_index] = {0, {}};
+
+    unsigned int position_index = GetPositionHash(pieces);
+    positions[position_index] = {true, 0, {}};
+
 
     uint64_t num_positions = 1;
 
@@ -176,11 +216,11 @@ int main() {
             // check if legal
 
             // position already looked at
-            std::array<std::array<int, 4>, kNumPieces> position_index = GetPositionIndex(next_position);
-            if (positions.contains(position_index)) {
+            unsigned int position_index = GetPositionHash(next_position);
+            if (positions[position_index].found) {
                 continue;
             }
-            positions[position_index] = {depth+1, {}};
+            positions[position_index] = {true, depth+1, {}};
             num_positions++;
 
 
