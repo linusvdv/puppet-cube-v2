eb1713a301cf568c22c1f99cbefaa9ae1c0f7a15
linusvdv <30822552+linusvdv@users.noreply.github.com>
2026-04-03 13:18:16 +0200

    better load balancing when using multiple gpus

--- src/search.cpp
+++ src/search.cpp
@@ -138,7 +138,7 @@ void DFSNextFrontierSearch (const State& state, Frontier& next_frontier,
         if (std::max(max_heuristic, uint8_t(Settings::GetTBDepth()+1)) + cur_depth + 1 >= depth - 3 &&
             depth - cur_depth - 1 - Settings::GetTBDepth() < 16 &&  // fits in the rotation registers
             cur_depth + 1 > 5 &&  // more than 5 moves need to be already made
-            depth - cur_depth - 1 - Settings::GetTBDepth() < 13) { // this value can be tweeked to have more cpu calculation needed
+            depth - cur_depth - 1 - Settings::GetTBDepth() < 10) { // this value can be tweeked to have more cpu calculation needed
 
             local_buffer->push_back({next_state.second, cur_depth + 1});
 
