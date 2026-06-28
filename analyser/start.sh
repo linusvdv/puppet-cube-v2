0eb8a4adebd368e76be13457759565ca566ff3b2
linusvdv <30822552+linusvdv@users.noreply.github.com>
2026-06-28 09:54:00 +0200

    remove directories analyser and optimizer

--- analyser/start.sh
+++
@@ -1,5 +0,0 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install numpy
-
-# cat ../out/out_heuristic_improve_m* | grep "moves:" | sed 's/.*moves: //' | sed -r 's/\x1b\[[0-9;]*m//g' > heuristic_improve.txt
